ac681cc22c53dc0aca30cd914711e7e68897fcf1
magezil <256@holbertonschool.com>
2019-07-31 21:10:27 +0000

    Make sure to not edit passed in arrays

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -12,4 +12,4 @@ def np_cat(mat1, mat2, axis=0):
         Return: new numpy.ndarray
     """
     import numpy as np
-    return np.concatenate((mat1, mat2), axis=axis)
+    return np.concatenate((mat1[:], mat2[:]), axis=axis)
