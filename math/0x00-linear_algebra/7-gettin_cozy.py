17d8adac8b13201d102e25a2f42ebfa5b71418e1
magezil <256@holbertonschool.com>
2019-07-31 01:46:39 +0000

    Add check if concatenate will work

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -10,11 +10,15 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
     new_mat = []
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         for row in mat1:
             new_mat.append(list(row))
         for row in mat2:
             new_mat.append(list(row))
     if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
         for row, element in zip(mat1, mat2):
             new_mat.append(list(row) + element)
     return new_mat
