334b97007b1d93aae32c37d81b7f45a532fa6993
magezil <256@holbertonschool.com>
2019-08-09 01:24:23 +0000

    Allow coefficient or polynomials to contain ints or floats

--- math/0x01-calculus/17-integrate.py
+++ math/0x01-calculus/17-integrate.py
@@ -6,12 +6,15 @@ def poly_integral(poly, C=0):
     """
         Calculate the integral of a polynomial represented by a list of
         coefficients
-        poly: list of integers
+        poly: list of numbers 
         C: int integral constant
 
         Return: list of integers, or None if poly or C is invalid
     """
-    if not type(poly) is list or not (type(C) is int or type(C) is float):
+    if not type(poly) is list or len(poly) == 0 or\
+            not (type(poly[0]) is int or type(poly[0] is float)):
+        return None
+    if not (type(C) is int or type(C) is float):
         return None
 
     integral = [C]
