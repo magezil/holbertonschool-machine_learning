d3c34a4435c837a6fe7c0a8e724fbca95da448cf
magezil <256@holbertonschool.com>
2019-08-09 01:19:10 +0000

    Update style

--- math/0x01-calculus/17-integrate.py
+++ math/0x01-calculus/17-integrate.py
@@ -4,14 +4,14 @@
 
 def poly_integral(poly, C=0):
     """
-        Calculate the integral of a polynomial represented by a list of coefficients
+        Calculate the integral of a polynomial represented by a list of
+        coefficients
         poly: list of integers
         C: int integral constant
 
         Return: list of integers, or None if poly or C is invalid
     """
-    if not type(poly) is list or len(poly) == 0 or type(poly[0]) is not int or \
-        type(C) is not int:
+    if not type(poly) is list or not (type(C) is int or type(C) is float):
         return None
 
     integral = [C]
